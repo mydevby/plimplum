7f4a240bb5473ffd5704fc3efc14deb33845350f
mydevby <mydev.by@gmail.com>
2023-01-20 13:52:04 +0300

    start add js

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,28 @@
-import * as flsFunctions from "./modules/functions.js";
+window.onload = function(){
+	let header = document.querySelector('.header');
+	
+	window.onscroll = function () {
+		let pageYOffset = window.pageYOffset;
 
-flsFunctions.isWebp();
+		if (pageYOffset > 0) {
+			//header.classList.add('header--scroll'); 
+		} else {
+			//header.classList.remove('header--scroll'); 
+		}
+    };
+
+	document.querySelectorAll('a[href^="#"').forEach(link => {
+		link.addEventListener('click', function(e) {
+			e.preventDefault();    
+			let href = this.getAttribute('href').substring(1);    
+			const scrollTarget = document.getElementById(href);    
+			const topOffset = header.offsetHeight;
+			const elementPosition = scrollTarget.getBoundingClientRect().top;
+			const offsetPosition = elementPosition - topOffset;
+			window.scrollBy({
+				top: offsetPosition,
+				behavior: 'smooth'
+			});
+		});
+	});
+};
