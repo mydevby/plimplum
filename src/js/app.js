48ae1457760d2039a079215184800309956b3c56
mydevby <mydev.by@gmail.com>
2023-01-20 15:54:07 +0300

    start js

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,26 @@
 window.onload = function(){
 	let header = document.querySelector('.header');
-	
+	let videoItems  = document.querySelectorAll('.video__item');
+	videoItems.forEach(item => {
+		item.addEventListener('click', function(e) {
+			let videoItemsActive = document.querySelector('.video__item--active');
+			videoItemsActive.classList.remove('video__item--active');
+			item.classList.add('video__item--active');
+		});
+	});	
+	// for (let i = 0; videoItems.length > i; i++) {
+	// 	videoItems[i].onclick = function() {
+	// 		let currentActive = videoItemsActive[0];
+	// 		console.log('work');
+	// 		if (currentActive) {
+	// 			currentActive.classList.remove('.video__item--active');
+	// 		}
+	// 		if (currentActive !== this) {
+	// 			this.classList.add('.video__item--active');
+	// 		}
+	// 	};
+	// }
+
 	window.onscroll = function () {
 		let pageYOffset = window.pageYOffset;
 
@@ -24,5 +44,17 @@ window.onload = function(){
 				behavior: 'smooth'
 			});
 		});
-	});
+	});	
+
+	const swiper = new Swiper('.js--video__list', {
+		direction: 'vertical',
+		slidesPerView: 3,
+		navigation: {
+		  nextEl: '.js--video__next',
+		  prevEl: '.js--video__prev',
+		},
+		scrollbar: {
+		  el: '.swiper-scrollbar',
+		},
+	  });
 };
